d52840919f8bdc7de474d57f4adcb354873a2dbb
Stephan Williams <stephanwilliams.93@gmail.com>
2013-08-21 20:41:49 -0400

    Initial Commit

---
+++ dnscript/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  dnscript
+//
+//  Created by Stephan Williams on 8/21/13.
+//  Copyright (c) 2013 Stephan Williams. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+

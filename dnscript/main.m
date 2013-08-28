240362addf4bd9a608911c8b6dc76e976a80b5b3
Stephan Williams <stephanwilliams.93@gmail.com>
2013-08-28 02:30:18 -0400

    config file will now be automatically reloaded when changed

--- dnscript/main.m
+++ dnscript/main.m
@@ -7,6 +7,8 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <sys/event.h>
+#import <fcntl.h>
 #import "NotificationHandler.h"
 
 #define kDNSCRIPT_CONFIG_FILE_NAME @".dnscriptrc"
@@ -23,17 +25,36 @@ int main(int argc, const char * argv[])
         [handler loadConfigFile:configFile];
         [handler startListening];
         
-        //TODO: listen for file change events? (kqueue API)
+        int queue = kqueue();
         
-        NSTimer *timer = [[NSTimer alloc] initWithFireDate:[[NSDate alloc] init]
-                                                  interval:0.1
-                                                    target:nil
-                                                  selector:nil
-                                                  userInfo:nil
-                                                   repeats:YES];
-        NSRunLoop *runLoop = [NSRunLoop currentRunLoop];
-        [runLoop addTimer:timer forMode:NSDefaultRunLoopMode];
-        [runLoop run];
+        const char *cstring_config_path = [[configFile path] cStringUsingEncoding:NSUTF8StringEncoding];
+        int config_fd = open(cstring_config_path, O_EVTONLY);
+        struct kevent kevp;
+        // You'd think just NOTE_WRITE would work, but editing with vim gives
+        // NOTE_RENAME and then NOTE_DELETE | NOTE_LINK. So, we register for
+        // NOTE_WRITE | NOTE_LINK. Someone else with the same problem:
+        // http://stackoverflow.com/questions/8293121/kqueues-on-mac-os-x-strange-event-order
+        EV_SET(&kevp, config_fd, EVFILT_VNODE, EV_ADD | EV_CLEAR, NOTE_WRITE | NOTE_LINK, 0, 0);
+        
+        kevent(queue, &kevp, 1, NULL, 0, NULL);
+        struct kevent event;
+        while (1) {
+            int n = kevent(queue, &kevp, 1, &event, 1, NULL);
+            if (n == 1) {
+                NSLog(@"%@ changed, reloading...", [configFile path]);
+                [handler reloadConfigFile];
+                
+                // Old file descriptor is no longer valid with the way vim saves
+                // (can you tell which program I'm using to test this?)
+                close(config_fd);
+                config_fd = open(cstring_config_path, O_EVTONLY);
+                kevp.ident = config_fd;
+            } else {
+                NSLog(@"Unexpected value: %d", n);
+            }
+        }
+        close(queue);
+        close(config_fd);
     }
     return 0;
 }
